1782d65d689baad72fdaaaa6176c0239b562f7aa
Francesco D'Angelo <frankdangelo55@gmail.com>
2025-04-17 16:55:16 +0200

    Lside and Rside defined through sqrt(A_0[g]/A_0[0])

--- lib/smear.c
+++ lib/smear.c
@@ -313,13 +313,13 @@ void Residual_Study(char out_res_study[], Real Corr_Z, PrecMatr Cov, PrecMatr gl
   for(int ilambda=0; ilambda<Nlambda; ilambda++)
   {
     fprintf(RS, "%s\t%s\t%s\t%s\n", conv(lambda(ilambda)).c_str(), conv(W_func_comp(0, Corr_Z, Cov, gl.row(ilambda), apar, A, f, Estar, sigma, norm, beta)/Target_Int(apar, Estar, sigma, norm, beta)).c_str(), conv(W_func_comp(1, Corr_Z, Cov, gl.row(ilambda), apar, A, f, Estar, sigma, norm, beta)).c_str(), conv((W_func_comp(1, Corr_Z, Cov, gl.row(ilambda), apar, A, f, Estar, sigma, norm, beta)/(W_func_comp(0, Corr_Z, Cov, gl.row(ilambda), apar, A, f, Estar, sigma, norm, beta))*Target_Int(apar, Estar, sigma, norm, beta))).c_str());
-    if(dLim<sqrt(W_func_comp(0, Corr_Z, Cov, gl.row(ilambda), 0, A0, f0, Estar, sigma, norm, beta)/W_func_comp(1, Corr_Z, Cov, gl.row(ilambda), 0, A0, f0, Estar, sigma, norm, beta)) and a==0)
+    if(dLim<sqrt(W_func_comp(0, Corr_Z, Cov, gl.row(ilambda), 0, A0, f0, Estar, sigma, norm, beta)/Target_Int(0, Estar, sigma, norm, beta)) and a==0)
     {
       lambda1 = lambda(ilambda-1);
       ilambda1=ilambda;
       a++;
     }
-    if(uLim<sqrt(W_func_comp(0, Corr_Z, Cov, gl.row(ilambda), 0, A0, f0, Estar, sigma, norm, beta)/W_func_comp(1, Corr_Z, Cov, gl.row(ilambda), 0, A0, f0, Estar, sigma, norm, beta)) and a==1)
+    if(uLim<sqrt(W_func_comp(0, Corr_Z, Cov, gl.row(ilambda), 0, A0, f0, Estar, sigma, norm, beta)/Target_Int(0, Estar, sigma, norm, beta)) and a==1)
     {
       lambda2 = lambda(ilambda-1);
       ilambda2=ilambda;
